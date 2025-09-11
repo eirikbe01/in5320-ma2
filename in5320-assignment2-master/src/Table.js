2de63128147c5305d51cfa68469166cf31834f90
Eirik Egge <eeg008@uib.no>
2025-09-11 14:19:44 +0200

    :sparkles: table component

--- in5320-assignment2-master/src/Table.js
+++ in5320-assignment2-master/src/Table.js
@@ -1,16 +1,34 @@
 
 
 function Table(props) {
-  console.log(props.apiData);
 
-  if (!props.apiData.results) {
+  const data = props.apiData.results
+  console.log(data);
+
+  if (!data) {
     // If the API request isn't completed return "loading...""
     return <p>Loading...</p>;
   } else {
-    // Write your code here:
-    return <table>
 
-    </table>;
+    // Write your code here:
+    return(
+      <table>
+        <tr>
+          <th>Country</th>
+          <th>Continent</th>
+          <th>Population</th>
+        </tr>
+        {data.map((row) => {
+          return (
+            <tr>
+              <td>{row.Country}</td>
+              <td>{row.Continent}</td>
+              <td>{row.Population}</td>
+            </tr>
+          );
+        })}
+      </table>
+    );
   }
 }
 
